0512d1c1a444a96b8695b9afb5c7deaa567d2ad1
Saurabh Singh <amreshsingh902681@gmail.com>
2025-10-28 11:56:30 +0530

    Final changes for A server Creating Files

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express'
+const app=express()
+
+app.get("/",(req,res)=>{
+    res.status(200).json({message:"Hello BhaiSahabh"})
+})
+
+const port=3000
+app.listen(port,()=>{
+    console.log("Server is Responding")
+})
