0c206a8e9e9629222c0113ba29cfd03a3e43a932
Saurabh Singh <amreshsingh902681@gmail.com>
2025-10-30 23:46:39 +0530

    Push New Changes

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
   res.status(200).json({ message: 'Hello BhaiSahabh' });
 });
 
+app.get('/', (res, res) => {
+  res.status(200).json({ message: 'Hello BhaiSahabh' });
+});
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log('Server is Responding');
