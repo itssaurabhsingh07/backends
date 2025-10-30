b21ba7ea457d5f96c3569e2b5270a6e585406dec
Saurabh Singh <amreshsingh902681@gmail.com>
2025-10-30 20:13:15 +0530

    Add all Changes of Eslint Pretier And CI

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import express from 'express'
-const app=express()
+import express from 'express';
+const app = express();
 
-app.get("/",(req,res)=>{
-    res.status(200).json({message:"Hello BhaiSahabh"})
-})
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Hello BhaiSahabh' });
+});
 
-const port=process.env.PORT || 5000
-app.listen(port,()=>{
-    console.log("Server is Responding")
-})
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log('Server is Responding');
+});
